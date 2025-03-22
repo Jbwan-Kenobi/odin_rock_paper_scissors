fafbf157801ebf1cd168dc9afa45108a7b180ca1
Jbwan-Kenobi <171935619+Jbwan-Kenobi@users.noreply.github.com>
2025-03-22 16:05:09 +0000

    Set value for getHumanChoice function

--- java.js
+++ java.js
@@ -2,12 +2,21 @@ function getComputerChoice() {
     let a = Math.floor(Math.random() * 100) +1;
     let computerChoice = ""
     if (a <= 33){
-        computerChoice = "Rock";
+        computerChoice = "rock";
     } else if (a > 33 && a <= 66){
-        computerChoice = "Paper";
-    } else computerChoice = "Scissors" 
+        computerChoice = "paper";
+    } else computerChoice = "scissors" 
     
     return computerChoice
 }
 
 console.log(getComputerChoice())
+
+function getHumanChoice() {
+    let humanChoice = prompt("Make your move! Rock, Paper or Scissors");
+    let lowerHumanChoice = humanChoice.toLowerCase();
+
+    return lowerHumanChoice
+}
+
+console.log(getHumanChoice())

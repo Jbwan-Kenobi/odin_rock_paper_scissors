6c16c10f2042d7ec287668fc3391cea1bac32156
Jbwan-Kenobi <171935619+Jbwan-Kenobi@users.noreply.github.com>
2025-03-22 20:41:42 +0000

    Set code and output for playRound function

--- java.js
+++ java.js
@@ -1,3 +1,6 @@
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
     let a = Math.floor(Math.random() * 100) +1;
     let computerChoice = ""
@@ -10,7 +13,7 @@ function getComputerChoice() {
     return computerChoice
 }
 
-console.log(getComputerChoice())
+/*console.log(getComputerChoice())*/
 
 function getHumanChoice() {
     let humanChoice = prompt("Make your move! Rock, Paper or Scissors");
@@ -19,4 +22,44 @@ function getHumanChoice() {
     return lowerHumanChoice
 }
 
-console.log(getHumanChoice())
+/*console.log(getHumanChoice())*/
+
+function playRound(computerChoice, humanChoice) {
+    let winner = ""
+    if(computerChoice == "rock" && humanChoice == "scissors" ||
+       computerChoice == "paper" && humanChoice == "rock" ||
+       computerChoice == "scissors" && humanChoice == "paper") {
+       winner = "computer";
+       }
+    else if(humanChoice == "rock" && computerChoice == "scissors" ||
+        humanChoice == "paper" && computerChoice == "rock" ||
+        humanChoice == "scissors" && computerChoice == "paper") {
+        winner = "human";
+        } else {
+            winner = "draw"
+        }
+
+    return winner
+}
+
+const computer = getComputerChoice();
+const human = getHumanChoice();
+
+let win = playRound(computer, human);
+
+if(win == "computer") {
+    console.log("You lose! " + computer + " beats " + human)
+} else if (win == "human"){
+    console.log("You win! " + human + " beats " + computer)
+} else {
+    console.log("It's a draw: " + human + " matches " + computer)
+}
+
+if(win == "computer"){
+    computerScore = computerScore + 1
+} else if(win == "human") {
+    humanScore = humanScore +1
+}
+
+console.log("Computer Score: " + computerScore)
+console.log("Human Score: " + humanScore)
